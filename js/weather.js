2be42d6c2ced5a54136a27ca48db546f1c6723d1
paipai-pai <1157286836@qq.com>
2022-11-25 14:34:13 +0800

    Site updated: 2022-11-25 14:34:11

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "4A86E8",
+        "tmpSize": "16",
+        "cityColor": "666666",
+        "citySize": "16",
+        "aqiColor": "FF9900",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "20",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "125",
+        "top": "20",
+        "key": "96c599729007443782f67ab781e048a2"
+    }
+}
