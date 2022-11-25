6069fb1b9a6936af64b606cd185148d7c78e5a33
paipai-pai <1157286836@qq.com>
2022-11-25 14:46:32 +0800

    Site updated: 2022-11-25 14:46:31

--- js/weather.js
+++ js/weather.js
@@ -1,24 +1,24 @@
 WIDGET = {
-    "CONFIG": {
-        "modules": "01234",
-        "background": "5",
-        "tmpColor": "4A86E8",
-        "tmpSize": "16",
-        "cityColor": "666666",
-        "citySize": "16",
-        "aqiColor": "FF9900",
-        "aqiSize": "16",
-        "weatherIconSize": "24",
-        "alertIconSize": "18",
-        "padding": "10px 10px 10px 10px",
-        "shadow": "0",
-        "language": "auto",
-        "borderRadius": "20",
-        "fixed": "true",
-        "vertical": "top",
-        "horizontal": "left",
-        "left": "200",
-        "top": "10",
-        "key": "96c599729007443782f67ab781e048a2"
-    }
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "4A86E8",
+    "tmpSize": "16",
+    "cityColor": "666666",
+    "citySize": "16",
+    "aqiColor": "FF9900",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "20",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "200",
+    "top": "10",
+    "key": "67340e706865487987fcb3fab6a6a2fd"
+  }
 }
